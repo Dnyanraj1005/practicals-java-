5d342db5ba05278ee9cfe954a9146c17ba0865a2
Dnyanraj1005 <155436819+Dnyanraj1005@users.noreply.github.com>
2024-05-08 11:58:49 +0530

    Delete File_Handling/FileClass.java

--- File_Handling/FileClass.java
+++
@@ -1,9 +0,0 @@
-import java.io.FileInputStream;
-
-class FileClass{
- public static void main(String args[])throws java.io.IOException{
- FileInputStream f = new FileInputStream("New");
- System.out.println("mmmmmmmmmmm");
- f.read();
- } 
- }
